25c09218d0d2f91f219faeb5f4299984875cd89e
Sunny Kumar <113859373+sunnykumar26@users.noreply.github.com>
2023-09-24 19:39:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://qwickchat.netlify.app'],
     methods: ['GET', 'POST']
   }
 })
